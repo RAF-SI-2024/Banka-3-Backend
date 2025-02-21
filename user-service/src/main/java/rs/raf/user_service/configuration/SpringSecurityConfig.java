3b4ce3e9ea5bb462822cef0b90d5f6bfba7b92a2
Djordje Zivkovic <djordje@zivkovic.me>
2025-02-21 17:17:17 +0100

    Inicijalizacija Spring projekta, PostgreSQL baze i Swagger-a

---
+++ user-service/src/main/java/rs/raf/user_service/configuration/SpringSecurityConfig.java
@@ -0,0 +1,34 @@
+package rs.raf.user_service.configuration;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("*")
+@EnableWebSecurity
+@EnableAsync
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().headers()
+                .frameOptions().disable();
+    }
+}
