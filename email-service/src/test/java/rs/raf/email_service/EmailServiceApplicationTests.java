4184542aaa366706c76e7b241f9b5f9142deed75
Djordje Zivkovic <djordje@zivkovic.me>
2025-02-23 21:10:42 +0100

    Temporarily remove empty integration tests

--- email-service/src/test/java/rs/raf/email_service/EmailServiceApplicationTests.java
+++ email-service/src/test/java/rs/raf/email_service/EmailServiceApplicationTests.java
@@ -3,7 +3,8 @@ package rs.raf.email_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// removed until integration tests are written
+//@SpringBootTest
 class EmailServiceApplicationTests {
 
     @Test
--- user-service/src/test/java/rs/raf/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/rs/raf/user_service/UserServiceApplicationTests.java
@@ -3,7 +3,8 @@ package rs.raf.user_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// removed until integration tests are written
+//@SpringBootTest
 class UserServiceApplicationTests {
 
     @Test
