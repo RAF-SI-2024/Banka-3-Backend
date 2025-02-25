6d47efdcf41d4345c4aa171005264745c0ee4335
Djordje Zivkovic <djordje@zivkovic.me>
2025-02-26 00:30:44 +0100

    Add JWT Auth to Swagger UI

--- user-service/src/main/java/rs/raf/user_service/configuration/SwaggerConfig.java
+++ user-service/src/main/java/rs/raf/user_service/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package rs.raf.user_service.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,14 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info().title("User Service")
                         .description("User Service API")
-                        .version("1.0"));
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
