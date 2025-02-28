0f4ceeb6405d1d0bde4361f667a2446e6416ed14
Djordje Zivkovic <djordje@zivkovic.me>
2025-02-28 03:07:52 +0100

    FIX PERMISSION ENDPOINT !!!!!!!!!

--- user-service/src/main/java/rs/raf/user_service/dto/PermissionRequestDto.java
+++ user-service/src/main/java/rs/raf/user_service/dto/PermissionRequestDto.java
@@ -2,11 +2,13 @@ package rs.raf.user_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PermissionRequestDto {
     private Long id;
 }
