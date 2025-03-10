e27e7528a8fcf3b3d32baef9e6d8a2eb0f5d17cf
David <dravic10121rn@raf.rs>
2025-03-10 21:34:48 +0100

    izracunavanje rate

--- bank-service/src/main/java/rs/raf/bank_service/repository/LoanRepository.java
+++ bank-service/src/main/java/rs/raf/bank_service/repository/LoanRepository.java
@@ -3,8 +3,12 @@ package rs.raf.bank_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rs.raf.bank_service.domain.entity.Loan;
+import rs.raf.bank_service.domain.enums.LoanStatus;
+
+import java.util.List;
 
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByStatus(LoanStatus loanStatus);
     // Možemo dodati custom metode ako su potrebne
 }
