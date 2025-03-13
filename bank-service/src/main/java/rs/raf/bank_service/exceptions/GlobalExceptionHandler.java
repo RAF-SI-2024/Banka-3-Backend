d97f6c3d7b24e7df3f54e8189f7afa260c0ed7d9
Djordje Zivkovic <djordje@zivkovic.me>
2025-03-13 06:19:11 +0100

    Add exception handlers again

--- bank-service/src/main/java/rs/raf/bank_service/exceptions/GlobalExceptionHandler.java
+++ bank-service/src/main/java/rs/raf/bank_service/exceptions/GlobalExceptionHandler.java
@@ -28,5 +28,17 @@ public class GlobalExceptionHandler {
         logger.error(errorMessage);
         return ResponseEntity.badRequest().body(new ErrorMessageDto(errorMessage));
     }
-    
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({PayeeNotFoundException.class, LoanRequestNotFoundException.class})
+    public ResponseEntity<ErrorMessageDto> handleNotFound(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessageDto(ex.getMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({AccountNotFoundException.class, CurrencyNotFoundException.class})
+    public ResponseEntity<ErrorMessageDto> handleBadRequest(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessageDto(ex.getMessage()));
+
+    }
     }
