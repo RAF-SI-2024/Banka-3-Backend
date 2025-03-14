684e01fc469badb4f6eec73133213ebf5dea951b
Djordje Zivkovic <djordje@zivkovic.me>
2025-03-14 15:24:19 +0100

    Add exchange, listing entities

---
+++ stock-service/src/main/java/rs/raf/stock_service/domain/entity/Option.java
@@ -0,0 +1,30 @@
+package rs.raf.stock_service.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import rs.raf.stock_service.domain.enums.OptionType;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@DiscriminatorValue("OPTION")
+public class Option extends Listing {
+    @Enumerated(EnumType.STRING)
+    private OptionType optionType;
+    
+    private BigDecimal strikePrice;
+    private BigDecimal impliedVolatility;
+    private Integer openInterest;
+    private LocalDate settlementDate;
+    
+    @ManyToOne
+    private Stock underlyingStock;
+}
