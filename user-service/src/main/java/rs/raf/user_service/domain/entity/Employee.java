28757c9326c332f37cce00768dca14f2b4b851bb
Djordje Zivkovic <djordje@zivkovic.me>
2025-03-21 01:02:00 +0100

    Fix delete employee with actuary limit

--- user-service/src/main/java/rs/raf/user_service/domain/entity/Employee.java
+++ user-service/src/main/java/rs/raf/user_service/domain/entity/Employee.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import java.util.Date;
 
 
@@ -28,6 +30,9 @@ public class Employee extends BaseUser {
     private String department;
 
     private boolean active;
+
+    @OneToOne(mappedBy = "employee", cascade = CascadeType.REMOVE)
+    private ActuaryLimit actuaryLimit;
 
     public Employee(String firstName, String lastName, Date birthDate, String gender, String email, String phone,
                     String address, String username, String position, String department, Boolean active,
