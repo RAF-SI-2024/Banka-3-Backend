09879c9daa11a269e64ce36d3b679ad92a0d35fa
Djordje Zivkovic <djordje@zivkovic.me>
2025-04-01 22:03:16 +0200

    Fix

--- stock-service/src/main/java/rs/raf/stock_service/bootstrap/BootstrapData.java
+++ stock-service/src/main/java/rs/raf/stock_service/bootstrap/BootstrapData.java
@@ -14,6 +14,7 @@ import rs.raf.stock_service.service.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
