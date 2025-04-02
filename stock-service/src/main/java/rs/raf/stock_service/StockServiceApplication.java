2896b8abd312f0ce1b7265959e4d093d06e34dbb
Djordje Zivkovic <djordje@zivkovic.me>
2025-04-03 01:32:13 +0200

    Move alpha vantage api key to .env

--- stock-service/src/main/java/rs/raf/stock_service/StockServiceApplication.java
+++ stock-service/src/main/java/rs/raf/stock_service/StockServiceApplication.java
@@ -1,5 +1,6 @@
 package rs.raf.stock_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +12,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class StockServiceApplication {
 
     public static void main(String[] args) {
+        if (System.getenv("ALPHAVANTAGE_API_KEY") == null) {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("ALPHAVANTAGE_API_KEY", dotenv.get("ALPHAVANTAGE_API_KEY"));
+            System.out.println("Loaded AlphaVantage API Key from env: " + dotenv.get("ALPHAVANTAGE_API_KEY"));
+
+        } else {
+            System.out.println("Loaded AlphaVantage API Key from env (docker): " + System.getenv("ALPHAVANTAGE_API_KEY"));
+        }
         SpringApplication.run(StockServiceApplication.class, args);
     }
 
