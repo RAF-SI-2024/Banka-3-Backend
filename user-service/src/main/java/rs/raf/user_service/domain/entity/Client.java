c3bdde891177bb5d5e1c6d040b93876543671b0e
Djordje Zivkovic <djordje@zivkovic.me>
2025-04-06 13:26:14 +0200

    fix

--- user-service/src/main/java/rs/raf/user_service/domain/entity/Client.java
+++ user-service/src/main/java/rs/raf/user_service/domain/entity/Client.java
@@ -10,6 +10,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
